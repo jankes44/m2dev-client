6b597ea0ba188ef1dd286d20622949d9b337dd3a
jankes44 <kalima12345@gmail.com>
2026-01-12 14:33:15 +0000

    idle hunting custom ui

--- assets/root/game.py
+++ assets/root/game.py
@@ -34,6 +34,7 @@ import uiPlayerGauge
 import uiCharacter
 import uiTarget
 import uiteleport
+import uiidlehunting
 
 # PRIVATE_SHOP_PRICE_LIST
 import uiPrivateShopBuilder
@@ -114,6 +115,9 @@ class GameWindow(ui.ScriptWindow):
 		self.__ServerCommand_Build()
 		self.__ProcessPreservedServerCommand()
 		self.teleport = uiteleport.TeleportWindow()
+		
+		# Idle Hunting Window - initialized in Open()
+		self.idleHuntingWnd = None
 
 	def __del__(self):
 		player.SetGameWindow(0)
@@ -166,28 +170,28 @@ class GameWindow(ui.ScriptWindow):
 			self.testPKMode.SetOutline()
 			self.testPKMode.Show()
 
-			self.testAlignment = ui.TextLine()
-			self.testAlignment.SetFontName(localeInfo.UI_DEF_FONT)
-			self.testAlignment.SetPosition(0, 35)
-			self.testAlignment.SetWindowHorizontalAlignCenter()
-			self.testAlignment.SetHorizontalAlignCenter()
-			self.testAlignment.SetFeather()
-			self.testAlignment.SetOutline()
-			self.testAlignment.Show()
-
-		self.__BuildKeyDict()
-		self.__BuildDebugInfo()
-
-		# PRIVATE_SHOP_PRICE_LIST
-		uiPrivateShopBuilder.Clear()
-		# END_OF_PRIVATE_SHOP_PRICE_LIST
+		self.testAlignment = ui.TextLine()
+		self.testAlignment.SetFontName(localeInfo.UI_DEF_FONT)
+		self.testAlignment.SetPosition(0, 35)
+		self.testAlignment.SetWindowHorizontalAlignCenter()
+		self.testAlignment.SetHorizontalAlignCenter()
+		self.testAlignment.SetFeather()
+		self.testAlignment.SetOutline()
+		self.testAlignment.Show()
+	
+		# Initialize Idle Hunting Window (after packs are loaded)
+		if not self.idleHuntingWnd:
+			self.idleHuntingWnd = uiidlehunting.IdleHuntingWindow()
+			self.idleHuntingWnd.Hide()
 
 		# UNKNOWN_UPDATE
 		exchange.InitTrading()
 		# END_OF_UNKNOWN_UPDATE
 
-		if debugInfo.IsDebugMode():
-			self.ToggleDebugInfo()
+		self.__BuildKeyDict()
+		self.__BuildDebugInfo()
+
+		self.ToggleDebugInfo()
 
 		## Sound
 		snd.SetMusicVolume(systemSetting.GetMusicVolume())
@@ -374,6 +378,7 @@ class GameWindow(ui.ScriptWindow):
 		onPressKeyDict[app.DIK_J]			= lambda : self.__PressJKey()
 		onPressKeyDict[app.DIK_H]			= lambda : self.__PressHKey()
 		onPressKeyDict[app.DIK_B]			= lambda : self.__PressBKey()
+		onPressKeyDict[app.DIK_K]			= lambda : self.ToggleIdleHuntingWindow()
 		onPressKeyDict[app.DIK_F]			= lambda : self.__PressFKey()
 		onPressKeyDict[app.DIK_TAB]			= lambda : self.__PressTabKey()
 
@@ -550,6 +555,13 @@ class GameWindow(ui.ScriptWindow):
 			self.ObjectNum.Hide()
 			self.ViewDistance.Hide()
 			self.PrintMousePos.Hide()
+	
+	def ToggleIdleHuntingWindow(self):
+		if self.idleHuntingWnd:
+			if self.idleHuntingWnd.IsShow():
+				self.idleHuntingWnd.Close()
+			else:
+				self.idleHuntingWnd.Open()
 
 	def __BuildDebugInfo(self):
 		## Character Position Coordinate
@@ -1832,7 +1844,11 @@ class GameWindow(ui.ScriptWindow):
 		self.interface.bigBoard.SetTip(message)
 
 	def BINARY_SetTipMessage(self, message):
-		self.interface.tipBoard.SetTip(message)		
+		self.interface.tipBoard.SetTip(message)
+	
+	def BINARY_Idle_Hunting_Info(self, state, mob_vnum, time_left, hunt_duration, max_daily_seconds, total_time_today):
+		if self.idleHuntingWnd:
+			self.idleHuntingWnd.OnUpdateState(state, mob_vnum, time_left, hunt_duration, max_daily_seconds, total_time_today)
 
 	def BINARY_AppendNotifyMessage(self, type):
 		if not type in localeInfo.NOTIFY_MESSAGE:
